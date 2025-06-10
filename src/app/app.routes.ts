1a9e0f44b7061ee436e7a4f975de339a6e3037ad
DUJARDIN Joséphine <josephine83230@gmail.com>
2025-06-10 21:21:14 +0200

    all pages and routes created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from '@angular/router';
-
+import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { LogementComponent } from './pages/logement/logement.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'a-propos', component: AboutComponent },
-  { path: 'logement/:logementId', component: LogementComponent },
-  { path: '**', component: NotFoundComponent }
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'a-propos', component: AboutComponent },
+      { path: 'logement/:logementId', component: LogementComponent },
+      { path: '**', component: NotFoundComponent }
+    ]
+  }
 ];
